401766c9f64d793438c1651d041a507d4e1d9ddb
Fredrick-peace-akinpelumi <108526101+Fredrick-peace-akinpelumi@users.noreply.github.com>
2023-02-28 23:40:56 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './component/pages/Login';
 import './App.css';
+import Casino from './component/pages/casino/Casino';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<Login/>}/>
+      <Route path='/casino' element={<Casino/>}/>
     </Routes>
     </>
   );
