595a6156c9451a066172f2db498227f1f804a647
Aremu Olakunle <107206801+COL-js@users.noreply.github.com>
2023-03-02 22:34:41 +0100

    finished the left sidebar of sport.jsx

--- src/component/LeftSidebar/LeftSidebar.jsx
+++ src/component/LeftSidebar/LeftSidebar.jsx
@@ -1,22 +1,22 @@
 import "./LeftSidebar.css";
 
 import search from "../images/search.png"
+import { LiveJackpot, PreMatchJackpot } from "../JackpotComponent/JackpotComponent";
+import { ActionMenu, Favourite, QuickMenu, SidebarSport, Support } from "../LeftSidebarComponent/LeftSidebarComponent";
 
-const LeftSidebar=()=>{
-    return(
-        <div className="LeftSidebar">
-            <form>
-                <input type="text" placeholder="" />
-                <button type="submit"><img src={search} alt="Search button" /></button>
-            </form>
-            <div>
-                <div>
-                    <span>LIVE JACKPOT</span>
-                </div>
-            </div>
-
-
-        </div>
-    )
-}
+const LeftSidebar=()=>(
+    <div className="LeftSidebar">
+        <form>
+            <input type="text" placeholder="" />
+            <button type="submit"><img src={search} alt="Search button" /></button>
+        </form>
+        <LiveJackpot />
+        <PreMatchJackpot />
+        <QuickMenu />
+        <Favourite />
+        <SidebarSport />
+        <Support />
+        <ActionMenu />
+    </div>
+)
 export default LeftSidebar;
