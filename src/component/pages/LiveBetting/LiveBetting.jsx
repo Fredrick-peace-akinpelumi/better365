e803d7ba0d7069bf79d85d96ce881f29ac5d4f65
Fredrick-peace-akinpelumi <108526101+Fredrick-peace-akinpelumi@users.noreply.github.com>
2023-03-05 23:56:56 +0100

    latest commit

--- src/component/pages/LiveBetting/LiveBetting.jsx
+++ src/component/pages/LiveBetting/LiveBetting.jsx
@@ -1,5 +1,9 @@
 import "./LiveBetting.css"
 import Navbar from "../../static/Navbar/Navbar";
+import SubNavbar from "../../SubNavbar/SubNavbar";
+import LivebetNav from "../../LiveBetNav/LivebetNav";
+import Livebetsub from "../../livebetsub/Livebetsub";
+import Livebetgames from "../../LivebetGames/Livebetgames";
 
 
 
@@ -7,7 +11,10 @@ const LiveBetting=()=>{
     return(
         <div className="LiveBetting">
             <Navbar/>
-            
+            <SubNavbar/>
+            <LivebetNav/>
+            <Livebetsub/>
+            <Livebetgames/>
 
         </div>
     )
---
+++ src/component/pages/virtual/Virtual.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../static/Navbar/Navbar'
+import './Virtual.css'
+
+const Virtual = () => {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default Virtual
